c1e825a620ec768a9a5b353c1e756ebbe9489fdc
waiting998 <xinbao.xu@innokin.com>
2020-10-15 19:34:11 +0800

    update home

---
+++ static/js/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {	
+
+    // Design list switching
+    $('.list-of-filter h5').click(function() {
+        var i = $('.list-of-filter h5').index(this);
+        $('.list-of-filter h5').removeClass('list-of-filter-active');
+        $('.list-of-filter h5').eq(i).addClass('list-of-filter-active');
+        $('.designs-list').css('display','none');
+        $('.designs-list').eq(i).css('display','grid');
+    });
+
+    // Button under banner
+    $('.banner-btn a').click(function(){
+        var i = $('.banner-btn a').index(this);
+        $(".banner-btn a").removeClass('list-of-filter-active');
+        $(".banner-btn a").eq(i).addClass('list-of-filter-active');
+        if(i===0){
+            $("html,body").animate({scrollTop:$("#get").offset().top},600);
+        }else if(i===1){
+            $("html,body").animate({scrollTop:$("#view").offset().top},600);
+        }
+    })
+
+});
