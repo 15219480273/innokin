ca8b60f4d13d94dcee4621c6af167ee6b022984b
waiting998 <xinbao.xu@innokin.com>
2020-10-16 14:11:59 +0800

    update home

--- src/design-tool/canvas/canvas.js
+++ src/design-tool/canvas/canvas.js
@@ -87,10 +87,7 @@ function Canvas() {
           <Bottom
             fillColor={bottomColor}
             style={{
-              position: "absolute",
-              top: "269px",
-              left: "43px",
-              width: "115px",
+
             }}
           />
           <FrontBottom
